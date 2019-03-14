5f7c9283bcf3caed95d6179fa04a642f2b97ba0c
Jakub Truneček <jakub.trunecek@gmail.com>
2019-03-14 12:08:13 +0100

    chore: more predefined

--- predefined.js
+++ predefined.js
@@ -11,5 +11,28 @@ export default {
             "Nose": {"color": "#ef843b"}
         }
     },
-    'Male': {}
+    'Male': {
+        'jakub.trunecek': {
+            "MaleHeadShape": {"color": "#FBD2B4"},
+            "Eyes": {"type": "A", "color": "#497665"},
+            "Mouth": {"type": "D", "color": "#ef843b"},
+            "MaleHair": {"type": "A", "color": "#A7856A"},
+            "MaleGlasses": {"type": "A", "color": "#9b59b6"},
+            "MaleClothes": {"type": "F", "color": "#1abc9c", "secondaryColor": "#16a085"},
+            "MaleAccessory": {"type": "B", "color": "#FFFFFF"},
+            "MaleFacialHair": {"type": "D"},
+            "Nose": {"color": "#ef843b"}
+        },
+        'trunda': {
+            "MaleHeadShape": {"color": "#FBD2B4"},
+            "Eyes": {"type": "A", "color": "#497665"},
+            "Mouth": {"type": "D", "color": "#ef843b"},
+            "MaleHair": {"type": "A", "color": "#A7856A"},
+            "MaleGlasses": {"type": "A", "color": "#9b59b6"},
+            "MaleClothes": {"type": "F", "color": "#1abc9c", "secondaryColor": "#16a085"},
+            "MaleAccessory": {"type": "B", "color": "#FFFFFF"},
+            "MaleFacialHair": {"type": "D"},
+            "Nose": {"color": "#ef843b"}
+        }
+    }
 }
